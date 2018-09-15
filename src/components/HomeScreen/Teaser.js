95c19296d61bc458b1a235d989645604b784019d
omeiregman <omeiregodsman@gmail.com>
2018-09-16 00:18:48 +0100

    teaser image

--- src/components/HomeScreen/Teaser.js
+++ src/components/HomeScreen/Teaser.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
 import './css/home.css';
+import img_coming from './img/coming-soon.jpg';
 
 
 const Teaser = () => {
     return(
         <section className="teaser">
-        <h1>The Crafters Connect</h1>
-        
-        <br></br>
-            <div className="teaser-info">
-                <h2>TCC Projects</h2>
-                <br></br>
-                <h3>To explore the world of Crafts, Celebrate and Connect Crafters, Promote Creativity and Craftpreneurship</h3>
-                <br></br>
-                <h4>Coming Soon ...</h4>
-            </div>
-            <br></br>
-            <p>TCC Magazine: <a href="https://www.thetccmaagazine.com" target="_blank">www.thetccmagazine.com</a></p>
+            <img src={img_coming} alt="Coming soon"/>
+            <p>TCC Magazine: <a href="https://www.thetccmagazine.com" target="_blank">www.thetccmagazine.com</a></p>
         </section>
     );
 }
